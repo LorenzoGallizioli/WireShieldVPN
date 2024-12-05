6310fa3ef364a72a8cffb8fcd30a374435e20ac4
LorenzoGallizioli <lorenzogallizioli23@gmail.com>
2024-12-05 14:52:28 +0100

    add test classes

---
+++ src/test/java/com/wireshield/wireguard/PeerManagerTest.java
@@ -0,0 +1,89 @@
+package com.wireshield.wireguard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PeerManagerTest {
+
+	@Test
+	public void testPeerManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPeer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemovePeer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPeers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/wireshield/wireguard/PeerTest.java
@@ -0,0 +1,104 @@
+package com.wireshield.wireguard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PeerTest {
+
+	@Test
+	public void testPeer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEndPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPublicKeyPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPresharedKeyPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllowedIps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
