f8b08d648fa24b0068b300e0507123cb96f86bf2
paganello <public.paganellithomas@gmail.com>
2024-12-11 07:37:52 +0100

    aggiunti file mancanti rispetto al main

---
+++ src/test/java/com/wireshield/av/ScanReportTest.java
@@ -0,0 +1,104 @@
+package com.wireshield.av;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScanReportTest {
+
+	@Test
+	public void testScanReport() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsThreatDetected() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetThreatDetails() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWarningClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsValidReport() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/wireshield/wireguard/ConnectionTest.java
@@ -0,0 +1,109 @@
+package com.wireshield.wireguard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConnectionTest {
+
+	@Test
+	public void testConnection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateTraffic() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateHandshakeTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSentTraffic() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetReceivedTraffic() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLastHandshakeTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPeerInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
