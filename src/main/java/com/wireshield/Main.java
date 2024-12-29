30df6fdafa770eab09faaf3253973f9a9d3b7de8
bnsdavide03 <bns.davide@gmail.com>
2024-12-29 17:16:51 +0100

    Sistemazione dimenticanza gestione eccezione AntivirusManager + implementazione Component Diagram

--- src/main/java/com/wireshield/Main.java
+++ src/main/java/com/wireshield/Main.java
@@ -5,8 +5,6 @@ import com.wireshield.localfileutils.SystemOrchestrator;
 
 import java.io.IOException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.json.simple.parser.ParseException;
 
 /*
